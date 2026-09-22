8e4aa98575685f0cedc6d0b2dc16fc78b9628c62
dev-cocarlos <carlosch021027@gmail.com>
2026-09-22 14:09:12 -0600

    Actualizacion de Settings

--- canaco/canaco/settings.py
+++ canaco/canaco/settings.py
@@ -127,7 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = 'home:login'
+LOGIN_REDIRECT_URL = 'home:index' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
